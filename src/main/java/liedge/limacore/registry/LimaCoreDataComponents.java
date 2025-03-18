7ca8e62c85f96ec9e7c162bdd650c91c0c34244b
Ed J <cuttingedge237@gmail.com>
2025-03-18 00:47:55 -0500

    Added owner data component Enabled encoding cache on relevant components

--- src/main/java/liedge/limacore/registry/LimaCoreDataComponents.java
+++ src/main/java/liedge/limacore/registry/LimaCoreDataComponents.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import liedge.limacore.LimaCommonConstants;
 import liedge.limacore.LimaCore;
 import liedge.limacore.network.LimaStreamCodecs;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
@@ -16,6 +17,7 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.List;
+import java.util.UUID;
 
 public final class LimaCoreDataComponents
 {
@@ -31,8 +33,9 @@ public final class LimaCoreDataComponents
     }
 
     // Standard data components
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<ItemContainerContents>> ITEM_CONTAINER = COMPONENTS.registerComponentType(LimaCommonConstants.KEY_ITEM_CONTAINER, builder -> builder.persistent(ItemContainerContents.CODEC).networkSynchronized(ItemContainerContents.STREAM_CODEC));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<ItemContainerContents>> ITEM_CONTAINER = COMPONENTS.registerComponentType(LimaCommonConstants.KEY_ITEM_CONTAINER, builder -> builder.persistent(ItemContainerContents.CODEC).networkSynchronized(ItemContainerContents.STREAM_CODEC).cacheEncoding());
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> ENERGY = COMPONENTS.registerComponentType(LimaCommonConstants.KEY_ENERGY_CONTAINER, builder -> builder.persistent(Codec.intRange(0, Integer.MAX_VALUE)).networkSynchronized(LimaStreamCodecs.varIntRange(0, Integer.MAX_VALUE)));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<UUID>> OWNER = COMPONENTS.registerComponentType(LimaCommonConstants.KEY_OWNER, builder -> builder.persistent(UUIDUtil.CODEC).cacheEncoding());
 
     // Enchantment data components
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<List<ConditionalEffect<ResourceKey<LootTable>>>>> EXTRA_LOOT_TABLE_EFFECT = ENCHANTMENT_COMPONENTS.registerComponentType("extra_loot_table", builder -> builder.persistent(ConditionalEffect.codec(ResourceKey.codec(Registries.LOOT_TABLE), LootContextParamSets.ENCHANTED_DAMAGE).listOf()));
