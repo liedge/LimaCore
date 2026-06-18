00e84030a36f3c0d867d69c0216152eeabb29801
Ed J <cuttingedge237@gmail.com>
2026-06-18 17:02:07 -0500

    Item model extents helper

--- src/main/java/liedge/limacore/client/LimaCoreClientUtil.java
+++ src/main/java/liedge/limacore/client/LimaCoreClientUtil.java
@@ -17,7 +17,11 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.chunk.LevelChunk;
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
 import org.jspecify.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 public final class LimaCoreClientUtil
 {
@@ -109,6 +113,34 @@ public final class LimaCoreClientUtil
     public static void setQuadParticleColor(SingleQuadParticle particle, LimaColor color)
     {
         particle.setColor(color.red(), color.green(), color.blue());
+    }
+
+    private static void calculateExtents(Consumer<Vector3fc> output, float x1, float y1, float z1, float x2, float y2, float z2)
+    {
+        output.accept(new Vector3f(x1, y1, z1));
+        output.accept(new Vector3f(x2, y1, z1));
+        output.accept(new Vector3f(x2, y1, z2));
+        output.accept(new Vector3f(x1, y1, z2));
+
+        output.accept(new Vector3f(x1, y2, z1));
+        output.accept(new Vector3f(x2, y2, z1));
+        output.accept(new Vector3f(x2, y2, z2));
+        output.accept(new Vector3f(x1, y2, z2));
+    }
+
+    public static void cubeExtents(Consumer<Vector3fc> output, float x1, float y1, float z1, float x2, float y2, float z2)
+    {
+        calculateExtents(output, x1 * 0.0625f, y1 * 0.0625f, z1 * 0.0625f, x2 * 0.0625f, y2 * 0.0625f, z2 * 0.0625f);
+    }
+
+    public static void cubeExtents(Consumer<Vector3fc> output, Vector3fc from, Vector3fc to)
+    {
+        cubeExtents(output, from.x(), from.y(), from.z(), to.x(), to.y(), to.z());
+    }
+
+    public static void sizedCubeExtents(Consumer<Vector3fc> output, float x, float y, float z, float xSize, float ySize, float zSize)
+    {
+        cubeExtents(output, x, y, z, x + xSize, y + ySize, z + zSize);
     }
     //#endregion
 }
